676bbddc5fe1217e76f0e71bd06cb083282dae97
Joseph Svoboda <joseph.svoboda15@yahoo.com>
2022-07-01 12:20:25 -0400

    Final Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿string dog = "Fido";
+int dogAges = 52;
+char chA = '\u0661';
+bool isAlive = false;
+double ageOfDog = 23.32;
+decimal dogLegs = 4;
+
+string dogName = "Ralph";
+int dogAge = 10;
+
+Console.WriteLine($"My dog's name is {dogName}, He is {dogAge} years old");
+
